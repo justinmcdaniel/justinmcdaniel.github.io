3d1309cec95d544a409e4e127eb5709e69be6a96
Justin McDaniel <justin.mcdaniel@protonmail.com>
2020-05-07 13:45:02 -0700

    Converted site to Angular 9.

--- scripts.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    var copyrightDateElement = document.getElementById("copyrightDate");
-
-    if (copyrightDateElement) {
-        var now = new Date();
-        copyrightDateElement.innerHTML = now.getFullYear();
-    }
-
-})();
